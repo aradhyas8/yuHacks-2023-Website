180c30d9868b4725ace39c40d2cfa785aa93f1ff
Tom Guo <gywghhb@gmail.com>
2023-05-25 20:29:41 -0400

    initial deployment

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // base: '/yuHacks-2023-Website',
-  plugins: [react()],
-})
